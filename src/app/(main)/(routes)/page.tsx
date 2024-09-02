c0f293e2b8932e807c514eb4872a26b8c252f198
tejasva-singh <tejasva.singh@copart.com>
2024-09-03 01:32:30 +0530

    added theme in next.js application

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,9 +1,12 @@
+import { ModeToggle } from "@/components/ui/theme-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="text-3xl font-bold ">
+      <p>aysu</p>
       <UserButton afterSwitchSessionUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -17,8 +18,18 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
