38ae45e8b811938d8779883fec874e92935ecc6f
singhTejasva <itejasvasingh@gmail.com>
2024-09-21 19:57:47 +0530

    setup brtanch

--- src/app/(main)/(routes)/abc/page.tsx
+++ src/app/(main)/(routes)/abc/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function ABC() {
   return (
     <div className="text-3xl font-bold ">
-      <p>ABC</p>
+      <p>DEvelop</p>
       <UserButton afterSwitchSessionUrl="/" />
       <ModeToggle />
     </div>
